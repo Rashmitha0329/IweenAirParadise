a0e30ce34bab17c721dc6e748ac24f25554def8a
taruntripgain <tarun@tripgain.com>
2025-10-15 10:58:25 +0530

    15-10-2015

--- src/test/java/com/iween/pageObject/BookingPage.java
+++ src/test/java/com/iween/pageObject/BookingPage.java
@@ -1,5 +1,39 @@
 package com.iween.pageObject;
 
-public class BookingPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.iween.utilities.ScreenshotUtil;
+
+public class BookingPage extends basePage{
+
+	public BookingPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+	@FindBy(xpath="//img[@class='logo-width']")
+	WebElement imageLogo;
+	
+	public void validateLogoInBookingPage(ExtentTest test) {
+		try {
+			if (imageLogo.isDisplayed()) {
+				test.log(Status.PASS, "Image is displayed on the Booking Page");
+			} else {
+				test.log(Status.FAIL, "Image is not displayed on the Booking Page");
+				ScreenshotUtil.captureAndAttachScreenshot1(driver, test, Status.FAIL, 
+						"Image is not displayed", "LogoImageIsBroken");
+			}
+		} catch (Exception e) {
+			test.log(Status.FAIL, "Exception occurred while validating logo: " + e.getMessage());
+			ScreenshotUtil.captureAndAttachScreenshot1(driver, test, Status.FAIL, 
+					"Exception while validating logo", "LogoValidationException");
+			 Assert.fail();
+		}
+	}
+
 
 }
